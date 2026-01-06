f031eef9a8b93f60869f9511d9e1474042d754aa
Artur <arturmineev9@mail.ru>
2026-01-06 17:09:36 +0300

    fixed detekt notes

--- app/src/main/java/ru/itis/f1app/presentation/MainScreen.kt
+++ app/src/main/java/ru/itis/f1app/presentation/MainScreen.kt
@@ -3,26 +3,20 @@ package ru.itis.f1app.presentation
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.List
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.navigator.tab.CurrentTab
 import cafe.adriel.voyager.navigator.tab.LocalTabNavigator
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabNavigator
-import cafe.adriel.voyager.navigator.tab.TabOptions
-import ru.itis.f1app.feature.races.impl.presentation.screen.races.RacesScreen
-import ru.itis.f1app.feature.standings.impl.presentation.StandingsScreen
+import ru.itis.f1app.presentation.tabs.RacesTab
+import ru.itis.f1app.presentation.tabs.StandingsTab
 
 class MainScreen : Screen {
     @Composable
@@ -51,52 +45,11 @@ private fun RowScope.TabNavigationItem(tab: Tab) {
     NavigationBarItem(
         selected = tabNavigator.current == tab,
         onClick = { tabNavigator.current = tab },
-        icon = { tab.options.icon?.let { Icon(painter = it, contentDescription = tab.options.title) } },
+        icon = {
+            tab.options.icon?.let {
+                Icon(painter = it, contentDescription = tab.options.title)
+            }
+        },
         label = { Text(tab.options.title) }
     )
-}
-
-
-object RacesTab : Tab {
-    override val options: TabOptions
-        @Composable
-        get() {
-            val title = "Races"
-            val icon = rememberVectorPainter(Icons.Filled.List)
-
-            return remember {
-                TabOptions(
-                    index = 0u,
-                    title = title,
-                    icon = icon
-                )
-            }
-        }
-
-    @Composable
-    override fun Content() {
-        RacesScreen().Content()
-    }
-}
-
-object StandingsTab : Tab {
-    override val options: TabOptions
-        @Composable
-        get() {
-            val title = "Standings"
-            val icon = rememberVectorPainter(Icons.Filled.Person)
-
-            return remember {
-                TabOptions(
-                    index = 1u,
-                    title = title,
-                    icon = icon
-                )
-            }
-        }
-
-    @Composable
-    override fun Content() {
-        StandingsScreen().Content()
-    }
 }
