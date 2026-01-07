c5d7af033136da7c0bb88f4628f2323f24c063fa
Arthur <arturmineev9@mail.ru>
2026-01-07 03:27:32 +0300

    added tests

---
+++ feature/drivers/impl/src/test/java/ru/itis/f1app/feature/drivers/impl/data/mapper/DriverDetailsMapperTest.kt
@@ -0,0 +1,4 @@
+package ru.itis.f1app.feature.drivers.impl.data.mapper
+
+class DriverDetailsMapperTest {
+}
---
+++ feature/drivers/impl/src/test/java/ru/itis/f1app/feature/drivers/impl/domain/usecase/GetDriverDetailsUseCaseImplTest.kt
@@ -0,0 +1,4 @@
+package ru.itis.f1app.feature.drivers.impl.domain.usecase
+
+class GetDriverDetailsUseCaseImplTest {
+}
